ed684e0053f55cc6ae3df9d5afc3c0a0abdac824
Neale <nealetham@tamu.edu>
2024-08-29 22:44:43 -0400

    refactor: book resources route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root "books#index"
 
-  get "books/index"
-  get "books/:id", to: "books#show"
+  resources :books
+  # get "books/index"
+  # get "books/:id", to: "books#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
