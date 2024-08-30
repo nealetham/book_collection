8906d271916c877d2ef9f34acda608bdb9b16cc6
Neale <nealetham@tamu.edu>
2024-08-30 15:10:09 -0400

    test: add integration test for author, price, pdate attributes

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -16,4 +16,30 @@ RSpec.describe 'Creating a book', type: :feature do
     expect(page).to have_content("Title can't be blank")
   end
 
+  scenario 'creating book with author' do
+    visit new_book_path
+    fill_in "book[title]", with: 'Harry Potter'
+    fill_in "book[author]", with: "J.K Rowling"
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('Harry Potter')
+  end
+
+  scenario 'creating book with price' do
+    visit new_book_path
+    fill_in "book[title]", with: 'Harry Potter'
+    fill_in "book[price]", with: "30"
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('Harry Potter')
+  end
+
+  scenario 'creating book with published date' do
+    visit new_book_path
+    fill_in "book[title]", with: 'Harry Potter'
+    fill_in "book[pdate]", with: "2024-08-30"
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('Harry Potter')
+  end
 end
