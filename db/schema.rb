6aa5e05cb4e968a1acecc3c43b178b8457f0a65a
Neale <nealetham@tamu.edu>
2024-08-30 15:32:53 -0400

    feat: add author, price, pdate to books with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_29_193813) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_30_193026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,5 +18,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_29_193813) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
+    t.decimal "price", precision: 10, scale: 2
+    t.date "pdate"
   end
 end
