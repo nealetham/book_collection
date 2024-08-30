d4ed6abff3ecfeb57911581f21a7cdae21902bc9
Neale <nealetham@tamu.edu>
2024-08-30 00:07:27 -0400

    feat: add flash notice to actions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,7 +15,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
 
     if @book.save
-      redirect_to @book
+      redirect_to(@book, notice: "Book successfully created.")
     else
       render :new, status: :unprocessable_entity
     end
@@ -29,7 +29,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
 
     if @book.update(book_params)
-      redirect_to @book
+      redirect_to(@book, notice: "Book successfully updated.")
     else
       render :edit, status: :unprocessable_entity
     end
@@ -38,8 +38,7 @@ class BooksController < ApplicationController
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-
-    redirect_to(root_path, status: :see_other)
+    redirect_to(books_path, status: :see_other, notice: "Book successfully deleted.")
   end
 
   private
