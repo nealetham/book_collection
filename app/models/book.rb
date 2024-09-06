adeedb0521e130475a3eb07fe1710d9e8e9e5c4f
Neale <nealetham@tamu.edu>
2024-09-06 10:57:51 -0400

    feat: add many-to-many book category association

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,5 @@ class Book < ApplicationRecord
   validates :author, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
   validates :pdate, presence: true
+  has_and_belongs_to_many :categories
 end
