b32581139c96afb635edf609527a1a7ef96eb473
Neale <nealetham@tamu.edu>
2024-09-08 14:04:56 -0400

    fix: user view tests

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -6,17 +6,19 @@ RSpec.describe 'users/index', type: :view do
   before(:each) do
     assign(:users, [
              User.create!(
-               username: 'Username'
+               username: 'Username1'
              ),
              User.create!(
-               username: 'Username'
+               username: 'Username2'
              )
            ])
   end
 
   it 'renders a list of users' do
     render
-    cell_selector = 'div>p'
-    assert_select cell_selector, text: Regexp.new('Username'.to_s), count: 2
+
+    assert_select 'table tbody tr', count: 3
+    assert_select 'table tbody tr td', text: 'Username1'
+    assert_select 'table tbody tr td', text: 'Username2'
   end
 end
