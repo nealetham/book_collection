64124945295dcabd4163ed853ce121b5313f3d4d
Neale <nealetham@tamu.edu>
2024-09-07 23:52:56 -0400

    chore: fix linting

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -3,52 +3,66 @@
 # location: spec/feature/integration_spec.rb
 require 'rails_helper'
 
-RSpec.describe 'Creating a book', type: :feature do
-  def fill_in_book_form(title:, author:, price:, pdate_year:, pdate_month:, pdate_day:)
+RSpec.shared_context 'book creation form setup' do
+  def fill_in_book_form(title:, author:, price:, pdate:)
     visit new_book_path
     fill_in 'book[title]', with: title
     fill_in 'book[author]', with: author
     fill_in 'book[price]', with: price
-
-    # Fill in the date using date_select
-    select pdate_day, from: 'book_pdate_3i' # day
-    select pdate_month, from: 'book_pdate_2i' # month
-    select pdate_year, from: 'book_pdate_1i' # year
-  end
-
-  scenario 'valid inputs' do
-    fill_in_book_form(title: 'Harry Potter', author: 'J.K Rowling', price: '25.0', pdate_year: '1997',
-                      pdate_month: 'June', pdate_day: '26')
+    select pdate[:day], from: 'book_pdate_3i'
+    select pdate[:month], from: 'book_pdate_2i'
+    select pdate[:year], from: 'book_pdate_1i'
     click_on 'Create Book'
+  end
+end
+
+RSpec.describe 'Create book with valid inputs', type: :feature do
+  include_context 'book creation form setup'
+
+  it 'valid title, author, price and date' do
+    fill_in_book_form(title: 'Harry Potter', author: 'J.K Rowling',
+                      price: '25.0', pdate: { year: '1997', month: 'June', day: '26' })
     visit books_path
     expect(page).to have_content('Harry Potter')
   end
+end
 
-  scenario 'creating book with missing title' do
-    fill_in_book_form(title: '', author: 'J.K Rowling', price: '25.0', pdate_year: '1997', pdate_month: 'June',
-                      pdate_day: '26')
-    click_on 'Create Book'
+RSpec.describe 'Create book with missing title', type: :feature do
+  include_context 'book creation form setup'
+
+  it 'missing title, valid otherwise' do
+    fill_in_book_form(title: '', author: 'J.K Rowling',
+                      price: '25.0', pdate: { year: '1997', month: 'June', day: '26' })
     expect(page).to have_content("Title can't be blank")
   end
+end
 
-  scenario 'creating book with missing author' do
-    fill_in_book_form(title: 'Harry Potter', author: '', price: '25.0', pdate_year: '1997', pdate_month: 'June',
-                      pdate_day: '26')
-    click_on 'Create Book'
+RSpec.describe 'Create book with missing author', type: :feature do
+  include_context 'book creation form setup'
+
+  it 'missing title, valid otherwise' do
+    fill_in_book_form(title: 'Harry Potter', author: '',
+                      price: '25.0', pdate: { year: '1997', month: 'June', day: '26' })
     expect(page).to have_content("Author can't be blank")
   end
+end
 
-  scenario 'creating book with missing price' do
-    fill_in_book_form(title: 'Harry Potter', author: 'J.K Rowling', price: '', pdate_year: '1997', pdate_month: 'June',
-                      pdate_day: '26')
-    click_on 'Create Book'
+RSpec.describe 'Create book with missing price', type: :feature do
+  include_context 'book creation form setup'
+
+  it 'missing price, valid otherwise' do
+    fill_in_book_form(title: 'Harry Potter', author: 'J.K Rowling',
+                      price: '', pdate: { year: '1997', month: 'June', day: '26' })
     expect(page).to have_content('Price is not a number')
   end
+end
 
-  scenario 'creating book with missing pdate' do
-    fill_in_book_form(title: 'Harry Potter', author: 'J.K Rowling', price: '25.0', pdate_year: '', pdate_month: '',
-                      pdate_day: '')
-    click_on 'Create Book'
+RSpec.describe 'Create book with missing pdate', type: :feature do
+  include_context 'book creation form setup'
+
+  it 'missing pdate, valid otherwise' do
+    fill_in_book_form(title: 'Harry Potter', author: 'J.K Rowling',
+                      price: '25.0', pdate: { year: '', month: '', day: '' })
     expect(page).to have_content("Pdate can't be blank")
   end
 end
